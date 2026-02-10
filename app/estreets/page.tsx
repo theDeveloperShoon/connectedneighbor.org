5e4fcf9f6635546d076579d14d9b5ae41878a65c
Sean Cotter <Seancotter04@gmail.com>
2026-02-09 19:37:42 -0500

    Refactor E-Streets page layout for improved structure and accessibility

--- app/estreets/page.tsx
+++ app/estreets/page.tsx
@@ -5,14 +5,20 @@ export default function EStreets() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-4xl font-bold mb-6">E-Streets</h1>
-        <a href="tel:973-382-2015" className="grid grid-cols-3 items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow">
+        <a href="tel:973-382-2015" className="flex flex-wrap items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow justify-between">
             <FaPhone className="w-5 h-5"/>
-            <p className="text-center text-sm">973-382-2015</p>
+            <div>
+                <h3 className="text-sm font-bold">Phone</h3>
+                <p className="text-center text-sm">973-382-2015</p>
+            </div>
             <FaArrowRight className="w-5 h-5 ml-auto"/>
         </a>
-        <a href="mailto:estreets@gmail.com" className="grid grid-cols-3 items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow">
+        <a href="mailto:estreets@gmail.com" className="flex flex-wrap items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow justify-between">
             <CiMail className="w-5 h-5"/>
-            <p className="text-center text-sm">estreets@gmail.com</p>
+            <div>
+                <h3 className="text-sm font-bold">Email</h3>
+                <p className="text-center text-sm">estreets@gmail.com</p>
+            </div>
             <FaArrowRight className="w-5 h-5 ml-auto"/>
         </a>
     </div>
