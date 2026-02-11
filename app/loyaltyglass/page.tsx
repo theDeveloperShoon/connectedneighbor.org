d995a16cbfa693b668601a073511ee79f090760a
Sean Cotter <Seancotter04@gmail.com>
2026-02-11 17:47:58 -0500

    Added Loyalty Glass page

---
+++ app/loyaltyglass/page.tsx
@@ -0,0 +1,31 @@
+import { CiMail } from "react-icons/ci";
+import { FaArrowRight, FaPhone } from "react-icons/fa";
+
+export const metadata = {
+    title: "Loyalty Glass",
+    description: "Loyalty Glass contact information",
+};
+
+export default function LoyaltyGlass() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold mb-6">Loyalty Glass</h1>
+        <a href="tel:201-870-6671" className="flex flex-wrap items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow justify-between">
+            <FaPhone className="w-5 h-5"/>
+            <div>
+                <h3 className="text-sm font-bold">Phone</h3>
+                <p className="text-center text-sm">(201) 870-6671</p>
+            </div>
+            <FaArrowRight className="w-5 h-5 ml-auto"/>
+        </a>
+        <a href="mailto:yuondy45@gmail.com" className="flex flex-wrap items-center mt-4 rounded-xl shadow-sm gap-4 py-4 px-4 w-7/8 sm:w-1/4 cursor-pointer hover:shadow-md transition-shadow justify-between">
+            <CiMail className="w-5 h-5"/>
+            <div>
+                <h3 className="text-sm font-bold">Email</h3>
+                <p className="text-center text-sm">yuondy45@gmail.com</p>
+            </div>
+            <FaArrowRight className="w-5 h-5 ml-auto"/>
+        </a>
+    </div>
+  );
+}
