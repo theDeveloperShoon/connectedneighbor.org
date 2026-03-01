0aa95be81853dc23e4864f5df2e6d2397c03b401
Sean Cotter <Seancotter04@gmail.com>
2026-03-01 01:36:17 -0500

    Add Contact, Privacy Policy, and Terms & Conditions pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PrivacyPolicy() {
+    return (
+        <div className="w-full h-full flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Privacy Policy</h1>
+        </div>
+    );
+}
