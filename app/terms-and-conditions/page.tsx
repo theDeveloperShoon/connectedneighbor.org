9c193195024aea7d4a840a37ef93ff12279b779c
Sean Cotter <Seancotter04@gmail.com>
2026-03-05 21:57:13 -0500

    Enhance Terms and Conditions page with comprehensive content and improved structure for better user understanding

--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,7 +1,100 @@
+import Header from "@/components/Header";
+
+
 export default function TermsAndConditions() {
     return (
-        <div className="w-full h-full flex items-center justify-center">
-            <h1 className="text-4xl font-bold">Terms & Conditions</h1>
+        <div className="min-h-screen ui-bg-secondary">
+            <Header />
+            <main className="flex min-h-screen flex-col py-16 px-16 sm:items-start">
+                <section className="pt-8">
+                    <h1 className="text-4xl font-bold ui-fg-primary-2">Terms & Conditions</h1>
+                    <h2 className="ui-fg-primary-2">Last Updated: March 5, 2026</h2>
+                </section>
+                <section className="py-6">
+                    <p className="ui-fg-primary-2 py-4">1. THIS DOCUMENT CONTAINS VERY IMPORTANT INFORMATION REGARDING YOUR RIGHTS 
+                        AND OBLIGATIONS, AS WELL AS CONDITIONS, LIMITATIONS, AND EXCLUSIONS THAT
+                        MAY APPLY TO YOU. PLEASE READ IT CAREFULLY.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">DISPUTES RATHER THAN JURY TRIALS.</p>
+                    <p className="ui-fg-primary-2 py-4">THESE TERMS REQUIRE THE USE OF ARBITRATION TO RESOLVE BY PLACING AN ORDER 
+                        FOR PRODUCTS OR SERVICES FROM THIS WEBSITE, YOU AFFIRM THAT YOU ARE OF LEGAL AGE TO ENTER INTO THIS 
+                        AGREEMENT, AND YOU ACCEPT AND ARE BOUND BY THESE TERMS AND CONDITIONS.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">YOU MAY NOT ORDER OR OBTAIN PRODUCTS OR SERVICES</p>
+                    <p className="ui-fg-primary-2 py-4">FROM THIS WEBSITE IF YOU (A) DO NOT AGREE TO THESE TERMS, (B) ARE NOT OF
+                        LEGAL AGE TO ENTER INTO THIS AGREEMENT, OR (C) ARE PROHIBITED FROM ACCESSING OR USING THIS WEBSITE
+                        OR ANY OF THIS WEBSITE'S CONTENTS, GOODS OR SERVICES BY APPLICABLE LAW.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">These terms and conditions "Terms" apply to the purchase and sale of products
+                        and services through ConnectedNeighbor.org "Site."  These Terms are subject to change by ConnectedNeigbor
+                         (refered to as "us", "we", or "our" as the context may require) without prior written notice at any time,
+                         in our sole discretion.  Any changes to the terms will be in effect as of the "Last Updated Date" referenced
+                         on the Site.  You should review these Terms prior to purchasing any product or services that are available
+                         through this Site.  Your continued use of this Site after the "Last Updated Date" will constitute your acceptance
+                         of and agreement to such changes
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">These Terms are an integral part of the Website Terms of Use that apply generally
+                        to the use of our Site.  You should also carefully review our Privacy Policy before placing an order for products
+                        or services through this Site (see Section 9).
+                    </p>
+                </section>
+                <section className="py-6">
+                    <p className="ui-fg-primary-2 py-4">
+                        2. Order Acceptance and Cancellation.  You agree that your order is an offer to buy, under these Terms, all products
+                        and services listed in your order.  All orders must be accepted by
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        us or we will not be obligated to sell the products or services to you.  We may choose not to accept orders at our sole
+                        discretion, even after we send you a confirmation email with your order number and details of the items you have ordered.
+                    </p>
+                </section>
+                <section className="py-6">
+                    <p className="ui-fg-primary-2 py-4">
+                        3. Prices and Payment Terms
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        (a) All prices, discounts, and promotions posted on this Site are subject to change without notice.  The price charged for a
+                        product or service will be the price in effect at the time the order is placed and will be set out in your order confirmation 
+                        email.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        Price increased will only apply to orders placed after such changes.  Posted prices do not include taxes or chargers for shipping
+                        and handling.  All such taxes and charges will be added to your merchandise total, and will be itemized in your shopping cart and 
+                        in your order confirmation email.  We strive to display accurate price information, however we may, on occasion, make inadvertent
+                        typographical errors, inaccuracies or omissions related to pricing and availability.  We reserve the right to correct any errors,
+                        inaccuracies, or omissions at any time and to cancel any orders arising from such occurences.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        If there is a conflict between the terms for a promotion and these Terms, the promotion terms will govern.
+                    </p>
+                    <p className="ui-fg-primary-2 py-2">
+                        (b) We may offer from time to time promotions on the site that may affect.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        (c) Terms of payment are within our sole disrection and payment must be recieved by us before acceptance of an order.
+                        You represent and warrant that (i) the credit card information you supply to us is ture, correct and complete, (ii) you are
+                        duly authorized to use such credit card for the purchase, (iii) charges incurred by you will be honored by your credit card 
+                        company, and (iv) you will pay charges incurred by you at the posted prices, including shipping and handling charges and all 
+                        applicable taxes, if any, regardless of the amount quoted on the Site at the time of the order.    
+                    </p>
+                </section>
+                <section className="py-6">
+                    <p className="ui-fg-primary-2 py-4">4. Shipment; Delivery; Title & Risk of Loss</p>
+                    <p className="ui-fg-primary-2 py-4">
+                        (a) We will arrange for shipment of the products to you.  Please check the individual
+                        product page for specific delivery options.  You will pay all shipping and handling charges for specific delivery options  
+                        Shipping and handling charges are reimbursement for the costs we incur in the processing, handling, packing, shipping and delivery
+                        of your order.
+                    </p>
+                    <p className="ui-fg-primary-2 py-4">
+                        (b) Title and risk of loss pass to you upon our transfer of hte products to the carrier/delivery. Shipping and delivery dates are 
+                        estimates only and cannot be guaranteed.  We are not liable for any delays in shipments.    
+                    </p>
+                </section>
+                <section>
+                    <p className="ui-fg-primary-2 py-4">5. Manufacturer's Warranty and Disclaimers</p>
+                </section>
+            </main>
         </div>
     );
 }
