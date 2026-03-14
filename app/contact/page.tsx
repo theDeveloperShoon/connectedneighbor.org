0480991360dfc6574126cc0090102fbfcc503541
Sean Cotter <Seancotter04@gmail.com>
2026-03-13 23:42:14 -0400

    Refactor Contact page layout to include Header and improve styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header";
+import QuoteForm from "@/components/QuoteForm";
+
 export default function Contact() {
     return (
-        <div className="w-full h-full flex items-center justify-center">
-            <h1 className="text-4xl font-bold">Contact Us</h1>
+        <div className="min-h-screen ui-bg-secondary">
+            <Header />
+            <main className="flex min-h-screen max-w-3xl flex-col py-32 px-16 sm:items-start">
+                <h1 className="text-6xl font-staatliches text-shadow-sm ui-fg-primary-2">Contact Us</h1>
+                <QuoteForm />
+            </main>
         </div>
     );
 }
